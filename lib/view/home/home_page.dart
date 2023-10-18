7a7a5f2a5ed518907e4962057c959b17087bb60e
tonmai <71057039+ekgaparp@users.noreply.github.com>
2023-10-18 13:09:00 +0700

    new appbar

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -2,6 +2,9 @@ import 'package:anim_search_bar/anim_search_bar.dart';
 import 'package:app_home_iot/view/tab/tab_living_room.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:weather/weather.dart';
+
+// enum AppState { NOT_DOWNLOADED, DOWNLOADING, FINISHED_DOWNLOADING }
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,144 +15,183 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController textController = TextEditingController();
+
+  String key = '0d67fcc57039eb5d6f167c8beec907c9';
+  late WeatherFactory ws;
+  @override
+  void initState() {
+    ws = WeatherFactory(key, language: Language.THAI);
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        shrinkWrap: true,
-        slivers: [
-          SliverAppBar(
-            centerTitle: true,
-            title: const Text(
-              'Home',
-              style: TextStyle(fontSize: 26),
-            ),
-            systemOverlayStyle:
-                const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-            shape: const ContinuousRectangleBorder(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40),
-                    bottomRight: Radius.circular(40))),
-            automaticallyImplyLeading: false,
-            expandedHeight: 200,
-            pinned: true,
-            floating: true,
-            leading: IconButton(
-              onPressed: () => Navigator.pop(context),
-              icon: Icon(
-                Icons.chevron_left,
-                color: Theme.of(context).primaryColor,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: CustomScrollView(
+          shrinkWrap: true,
+          slivers: [
+            SliverAppBar(
+                centerTitle: true,
+                title: const Text('Home', style: TextStyle(fontSize: 26)),
+                systemOverlayStyle: const SystemUiOverlayStyle(
+                    statusBarColor: Colors.transparent),
+                shape: const ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(30),
+                        bottomRight: Radius.circular(30))),
+                automaticallyImplyLeading: false,
+                expandedHeight: 180,
+                pinned: true,
+                toolbarHeight: 70,
+                floating: true,
+                flexibleSpace: FlexibleSpaceBar(
+                    background: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(12.0),
+                          bottomRight: Radius.circular(12.0)),
+                      gradient: LinearGradient(
+                        
+                          colors: [
+                            Colors.yellow.shade700,
+                            Colors.redAccent,
+                          ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          tileMode: TileMode.repeated)),
+                  child: _buildWeatherLocate(),
+                ))),
+            SliverFillRemaining(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildRoomButton(),
+                  ],
+                ),
               ),
-            ),
-            //
-            flexibleSpace: FlexibleSpaceBar(
-                background: Container(
-                    decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(20.0),
-                            bottomRight: Radius.circular(20.0))),
-                    child: Container())),
-          ),
-          SliverFillRemaining(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                // children: List<int>.generate(100, (index) => index)
-                //     .map((index) => Container(
-                //           height: 40,
-                //           margin: const EdgeInsets.symmetric(vertical: 10),
-                //           color: Colors.grey[300],
-                //         alignment: Alignment.center,
-                //           child: Text('$index item'),
-                //         ))
-                //     .toList(),
-                children: [
-                  _buildSearchLocation(),
-                  _buildWeather(),
-                  _buildRoomButton(),
-                ],
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 
-  Widget _buildSearchLocation() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        AnimSearchBar(
-          onSubmitted: (String) {
-            print('String :$String');
-          },
-          onSuffixTap: (){
-            setState(() {
-            textController.clear();
-          });
-          },
-          textController: textController,
-          width: 370,
-        )
-      ],
+  StreamBuilder<Weather> _buildWeatherLocate() {
+    return StreamBuilder<Weather>(
+      stream: ws.currentWeatherByCityName('Bangkok, Thailand').asStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final weather = snapshot.data;
+
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100.0,
+                  margin: const EdgeInsets.only(
+                      top: 110.0, left: 16.0, right: 20.0),
+                  decoration: BoxDecoration(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10.0)),
+                      gradient: LinearGradient(
+                          colors: [
+                            Colors.yellow.shade700,
+                            Colors.redAccent,
+                          ],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          tileMode: TileMode.clamp)),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: Icon(
+                              Icons.cloud,
+                              color: Colors.white70,
+                              size: 40,
+                            ),
+                          ),
+                          Expanded(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                '${weather!.areaName}',
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Colors.white70,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 8.0,
+                              ),
+                              Text(
+                                '${weather.weatherDescription}',
+                                style: const TextStyle(
+                                    fontSize: 12.0, color: Colors.white70),
+                              ),
+                            ],
+                          )),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 10.0, right: 10.0),
+                            child: Text(
+                              '${weather.temperature?.celsius?.toStringAsFixed(2)}°C',
+                              style: const TextStyle(
+                                  fontSize: 26.0, color: Colors.white70),
+                            ),
+                          )
+                        ],
+                      ),
+                      // _buildSearchLocation()
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text('Error getting weather data: ${snapshot.error}'),
+          );
+        } else {
+          return const Center(
+            child: SizedBox(),
+          );
+        }
+      },
     );
   }
 
-  Widget _buildWeather() {
-    return Container(
-      height: 100.0,
-      margin: const EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-          gradient: LinearGradient(
-              colors: [Colors.yellow.shade700, Colors.redAccent],
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-              tileMode: TileMode.clamp)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Icon(
-              Icons.cloud,
-              color: Colors.white70,
-            ),
-          ),
-          Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const <Widget>[
-              Text(
-                'New York',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white70,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Text(
-                'Sunny',
-                style: TextStyle(fontSize: 12.0, color: Colors.white70),
-              ),
-            ],
-          )),
-          const Padding(
-            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-            child: Text(
-              '12°',
-              style: TextStyle(fontSize: 30.0, color: Colors.white70),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+  // Widget _buildSearchLocation() {
+  //   return Row(
+  //     mainAxisAlignment: MainAxisAlignment.end,
+  //     children: [
+  //       AnimSearchBar(
+  //         onSubmitted: (String) {
+  //           print('String :$String');
+  //         },
+  //         onSuffixTap: () {
+  //           setState(() {
+  //             textController.clear();
+  //           });
+  //         },
+  //         textController: textController,
+  //         width: MediaQuery.of(context).size.width * 0.9,
+  //       )
+  //     ],
+  //   );
+  // }
 
   Widget _buildRoomButton() {
     return Column(
